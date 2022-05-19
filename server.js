d86965b80a0702ffbafa71df2e8568533328ae48
diosvo <vtmn1212@gmail.com>
2022-05-19 11:54:40 +0700

    chore: install body-parser

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { PORT } = require("./config");
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 app.use((_, response, next) => {
   response.header("Access-Control-Allow-Origin", "*");
   response.header(
@@ -22,9 +24,11 @@ app.get("/", async (_, response) => {
 /* declared routes */
 
 const photosRoute = require("./routes/photos");
+const emailRoute = require("./bull/events/email.route");
 
 /* use those api now !! */
 
+app.use("/email", emailRoute);
 app.use("/photos", photosRoute);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
