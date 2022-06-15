78068449e306b52fee2e31ac3a1ab2e965e70578
Dios Vo <vtmn1212@gmail.com>
2022-06-15 13:59:29 +0700

    feat (photos): add more queries to execute
    - update swagger json and its controllers

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -3,43 +3,49 @@ const { ENDPOINT, DEFAULT_EXPIRATION } = require("../config");
 const axios = require("axios");
 
 const slug = "photos";
-const redisKey = "photo";
 
 const controller = {
   all: async (request, response) => {
-    const { _limit } = request;
-    const cache = await client.get(`${slug}`);
+    const { _limit = 10, albumId = null } = request.query;
+    const queries = `${ENDPOINT}/${slug}?_limit=${_limit}`;
+
+    const key = `${slug}:albumId:${albumId ?? "all"}:limit:${_limit}`;
+    const cache = await client.get(`${key}`);
 
     if (cache) {
       return response.json(JSON.parse(cache));
     }
 
     const { data } = await axios.get(
-      `${ENDPOINT}/${slug}?_limit=${_limit ?? 1}`
+      albumId && albumId > 0 ? queries.concat(`&albumId=${albumId}`) : queries
     );
-    client.setEx(`${slug}`, DEFAULT_EXPIRATION, JSON.stringify(data));
 
+    client.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(data));
     return response.json(data);
   },
 
   byId: async (request, response, _) => {
     const { id } = request.params;
-    const cache = await client.get(`${redisKey}-${id}`);
+    const cache = await client.get(`photo:${id}`);
 
     if (cache) {
       return response.json(JSON.parse(cache));
     }
 
     const { data } = await axios.get(`${ENDPOINT}/${slug}/${id}`);
-    client.setEx(`${redisKey}-${id}`, DEFAULT_EXPIRATION, JSON.stringify(data));
+    client.setEx(`photo:${id}`, DEFAULT_EXPIRATION, JSON.stringify(data));
 
     return response.json(data);
   },
 
-  create: async (request, response, _) => {
-    const { key, value } = request.body;
+  create: async (request, response, __) => {
     const { data } = await axios.post(`${ENDPOINT}/${slug}`);
-    await client.set(`${key}-${data["id"]}`, value);
+    const created = { ...request.body, ...data };
+    await client.setEx(
+      `photo:${data["id"]}`,
+      DEFAULT_EXPIRATION,
+      JSON.stringify(created)
+    );
     return response.json(data);
   },
 };
