353a9cd6d938f5912b662aa23c8d50e149232a23
diosvo <vtmn1212@gmail.com>
2022-06-16 10:00:31 +0700

    feat (swagger): accept nullable value
    - remove redis cache when filtering data

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -3,24 +3,13 @@ const { ENDPOINT, DEFAULT_EXPIRATION } = require("../config");
 const axios = require("axios");
 
 const slug = "photos";
+const URL = `${ENDPOINT}/${slug}`;
 
 const controller = {
   all: async (request, response) => {
-    const { _limit = 10, albumId = null } = request.query;
-    const queries = `${ENDPOINT}/${slug}?_limit=${_limit}`;
+    const queries = new URLSearchParams(request.query).toString();
+    const { data } = await axios.get(`${URL}?${queries}`);
 
-    const key = `${slug}:albumId:${albumId ?? "all"}:limit:${_limit}`;
-    const cache = await client.get(`${key}`);
-
-    if (cache) {
-      return response.json(JSON.parse(cache));
-    }
-
-    const { data } = await axios.get(
-      albumId && albumId > 0 ? queries.concat(`&albumId=${albumId}`) : queries
-    );
-
-    client.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(data));
     return response.json(data);
   },
 
@@ -32,14 +21,14 @@ const controller = {
       return response.json(JSON.parse(cache));
     }
 
-    const { data } = await axios.get(`${ENDPOINT}/${slug}/${id}`);
+    const { data } = await axios.get(`${URL}/${id}`);
     client.setEx(`photo:${id}`, DEFAULT_EXPIRATION, JSON.stringify(data));
 
     return response.json(data);
   },
 
   create: async (request, response, __) => {
-    const { data } = await axios.post(`${ENDPOINT}/${slug}`);
+    const { data } = await axios.post(`${URL}`);
     const created = { ...request.body, ...data };
     await client.setEx(
       `photo:${data["id"]}`,
