0c07f5ad40f49fa583cfd97d8683fa97ae98ac5e
Dios Vo <vtmn1212@gmail.com>
2023-05-02 14:18:36 +0700

    feat(queue): create env variable and its logs

---
+++ configs/queue-handler.js
@@ -0,0 +1,40 @@
+const BgGray = "\x1b[100m%s\x1b[0m";
+const BgGreen = '\x1b[102m%s\x1b[0m';
+const BgRed = '\x1b[41m%s\x1b[0m';
+
+const PREFIX = '[queue]';
+
+const handlerCompleted = (job) => {
+  console.info(BgGreen, PREFIX, `job in ${job.queue.name} completed for ${job.data.message}`);
+  job.remove();
+}
+
+const handlerFailure = (job, error) => {
+  if (job.attemptsMade >= job.opts.attempts) {
+    console.info(
+      BgRed,
+      PREFIX,
+      `job failures above threshold in ${job.queue.name} for ${job.id} \n\terror: ${error.message}`
+    );
+    return null;
+  }
+
+  console.info(
+    BgRed,
+    PREFIX,
+    `job in ${job.queue.name} failed for ${job.id}
+    error: ${error.message}. 
+    attempts left: ${job.opts.attempts - job.attemptsMade}
+    `
+  );
+}
+
+const handlerStalled = (job) => {
+  console.info(
+    BgGray,
+    PREFIX,
+    `job in ${job.queue.name} stalled for ${job.id}`
+  );
+}
+
+module.exports = { handlerCompleted, handlerFailure, handlerStalled }
--- queues/email.queue.js
+++ queues/email.queue.js
@@ -3,6 +3,8 @@ const email_process = require("../processes/email.process");
 const { BullAdapter } = require("@bull-board/api/bullAdapter");
 const { ExpressAdapter } = require("@bull-board/express");
 const { createBullBoard } = require("@bull-board/api");
+const { QUEUE_ATTEMPTS, QUEUE_REMOVE_ON_COMPLETE, QUEUE_REMOVE_ON_FAIL } = require("../config");
+const { handlerFailure, handlerCompleted, handlerStalled } = require('../configs/queue-handler');
 
 const adapter = new ExpressAdapter();
 const email_queue = queue("email");
@@ -16,8 +18,14 @@ email_queue.process(email_process);
 
 const send_email = (data) => {
   email_queue.add(data, {
-    attempts: 2, // If job fails it will retry till 2 times    
+    attempts: QUEUE_ATTEMPTS,
+    removeOnComplete: QUEUE_REMOVE_ON_COMPLETE,
+    removeOnFail: QUEUE_REMOVE_ON_FAIL,
   });
 };
+
+email_queue.on('failed', handlerFailure);
+email_queue.on('completed', handlerCompleted);
+email_queue.on('stalled', handlerStalled);
 
 module.exports = { send_email, adapter };
